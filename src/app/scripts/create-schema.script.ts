beebc3d7b86d7f36256e4f9f517e4d149c59bf58
vikei <vikei.dev@gmail.com>
2020-03-21 13:24:40 +0200

    Add script to generate type def

---
+++ src/app/scripts/create-schema.script.ts
@@ -0,0 +1,9 @@
+import path from 'path'
+import { emitSchemaDefinitionFile } from 'type-graphql'
+import { createSchema } from '../lib/create-schema'
+
+async function init() {
+  emitSchemaDefinitionFile(path.resolve(__dirname, '../schema.gql'), await createSchema())
+}
+
+init()
